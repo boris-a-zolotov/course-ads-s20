996ac014b7fa0d046911da8567b84037e24d43d9
Boris Zolotov <boris.a.zolotov@yandex.com>
2020-06-18 23:47:52 +0300

    priority queue figure

--- priority queue/pqueue.hs
+++ priority queue/pqueue.hs
@@ -16,14 +16,20 @@ queueProcess (-1:ls) x y = queueProcess ls (deleteMin x) (y+1)
 queueProcess (q:ls) x y = queueProcess ls (queueInsert q y x) (y+1)
 
 totikz :: (Integer,Integer) -> String -> Integer -> String
-totikz (a,b) col t = printf "\n  \\draw (%d,%d) -- (%d,%d); \\draw[%s] (%d,%d) -- (%d,0);" b a t a col t a t
+totikz (a,b) col t = printf
+  "\n        \\draw[thick] (%d,0.7 * %d) -- (%d,0.7 * %d);        \\draw[%s,very thick] (%d,0.7 * %d) -- (%d,0);"
+  b a t a col t a t
 
 -- Цвет || Элементы || Текущий список с временами || Номер шага || tikz-код
 queueProcessTikz :: String -> [Integer] -> [(Integer,Integer)] -> Integer -> String -> String
-queueProcessTikz col [] x y s = s ++ (foldl (++) "" (map (\r -> totikz r col y) x)) ++ "  }"
-queueProcessTikz col (-1:ls) x y s = queueProcessTikz col ls (deleteMin x) (y+1) (s ++ (totikz (head x) col y))
-queueProcessTikz col (q:ls) x y s = queueProcessTikz col ls (queueInsert q y x) (y+1) s
+queueProcessTikz color [] x step tikz =
+  (foldl (++) tikz (map (\r -> totikz r color step) x))
+  ++ "  }"
+queueProcessTikz color (-1:ls) x step tikz =
+  queueProcessTikz color ls (deleteMin x) (step+1) (tikz ++ (totikz (head x) color step))
+queueProcessTikz color (q:ls) x step tikz =
+  queueProcessTikz color ls (queueInsert q step x) (step+1) tikz
 
 main=do
-  putStrLn $ queueProcessTikz "red" [30,-1,18,11,14,-1,-1,-1,1,9,3,-1,-1,6,1000,29,-1,22,7,23,25,2,16,24,-1,27,10,-1,-1,-1,28,-1,-1,5,13,21,17,19,-1,-1,15,12,26,-1,-1,20,-1,8] [] 0 "\\tikz[scale=0.3]{"
-  putStrLn $ queueProcessTikz "blue" [30,-1,18,11,14,-1,-1,-1,1,9,3,-1,-1,6,4,29,-1,22,7,23,25,2,16,24,-1,27,10,-1,-1,-1,28,-1,-1,5,13,21,17,19,-1,-1,15,12,26,-1,-1,20,-1,8] [] 0 "\\tikz[scale=0.3]{"
+  putStrLn $ queueProcessTikz "oldq" [31,-1,18,11,14,-1,-1,-1,1,9,3,-1,-1,6,1000,29,-1,22,7,23,25,2,16,24,-1,27,10,-1,-1,-1,28,-1,-1,30,5,13,21,17,19,-1,-1,15,12,26,-1,-1,20,-1,8] [] 0 "\\tikz[scale=0.28]{"
+  putStrLn $ queueProcessTikz "newq" [31,-1,18,11,14,-1,-1,-1,1,9,3,-1,-1,6,4,29,-1,22,7,23,25,2,16,24,-1,27,10,-1,-1,-1,28,-1,-1,30,5,13,21,17,19,-1,-1,15,12,26,-1,-1,20,-1,8] [] 0 "\\tikz[scale=0.28]{"
